823747f2a6040fc13882c2112595c72fd9b22826
Atanas Vasilev <39429743+atanas-vasilev@users.noreply.github.com>
2022-03-30 12:55:32 +0000

    Completed main points from the assignment.

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
-function createGrid(size = 16) {
+let gridSize = 16; //Default
+
+function createGrid(x = gridSize) {
     let result = document.createElement("div");
     result.classList.add("draw-board")
-    for (let row = 0; row <= size; row++) {
+    for (let row = 1; row <= x; row++) {
         //create row
         let newRow = document.createElement("div");
         newRow.classList.add("row");
         newRow.classList.add("row-"+row);
 
-        for (let col = 0; col <= size; col++) {
+        for (let col = 1; col <= x; col++) {
             let newCol = document.createElement("div");
             newCol.classList.add("col");
             newCol.classList.add("col-"+col);
@@ -16,9 +18,26 @@ function createGrid(size = 16) {
         }
         result.append(newRow);
     }
+    document.querySelector("#draw-area").innerHTML = "";
     document.querySelector("#draw-area").append(result);
 }
 
+function attachEventListeners(){
+    document.querySelectorAll(".col").forEach(
+        cell => cell.addEventListener("mouseover", function(e){e.target.classList.add('painted')})
+    )
+}
 
+function resetDrawBoard() {
+    createGrid();
+    attachEventListeners()
+}
 
-createGrid()
+document.querySelector("#clear-btn").addEventListener("click",resetDrawBoard);
+document.querySelector("#grid-btn").addEventListener("click", () => {
+    gridSize = Number(prompt("What should the new size of the grid be? eg. 32"))
+    //TO-DO: ERROR HANDLING AND LIMIT TO 100
+    resetDrawBoard()
+});
+
+resetDrawBoard();
